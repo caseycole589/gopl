9b78ede5e12291f3c7201658052cd9d499c8d50f
casey <caseycole589@gmail.com>
2015-12-13 16:49:09 -0500

    finishing up first prgram of chapter 3 wanted to commit so I didn't lose any work

---
+++ src/chapter3/polygon_svg/svg_1.go
@@ -0,0 +1,36 @@
+//surface computes an svg rendering of a 3-d surface function
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+const (
+	width, height 	= 600,200	//canvas size in pixels
+	cells		= 100		//number of grid cells
+	xyrange 	= 30.0 		// axis ranges (-xyrange..+xyrange)
+	xyscale		= width / 2 / xyrange //pixels per x or y unit
+	zscale		= height * 0.4 	//pixels per z unit
+	angle		= math.Pi / 6 	//angle of x,y axes(=30 degrees)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) //sin(30 degrees), cos(30 degrees)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' " +
+		"style='stroke: grey; fill: white; stroke-width: 0.7' " +
+		"width='%d' height='%d'>", width, height)
+
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			fmt.Printf("<polygon points='%g %g %g %g %g %g %g %g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}	
+	fmt.Println("</svg>")
+}
